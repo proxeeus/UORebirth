c5a75981089060e1fad4f7e34d50ae93c9e70849
proxeeus <proxeeus@gmail.com>
2020-04-07 22:43:19 +0200

    rework SpawnerDev into Spawn.cs

--- Scripts/Commands/Spawn.cs
+++ Scripts/Commands/Spawn.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using Server.Mobiles;
 using Server.Commands;
+using System.Reflection;
 
 // Version 0.8
 // Usage:
@@ -14,16 +15,16 @@ using Server.Commands;
 
 namespace Server.Scripts.Commands
 {
-    public class SpawnerDev
+    public class Spawn
 	{
 		private static int m_Count;
 
 		public static void Initialize()
 		{
-			CommandSystem.Register( "SpawnerDev", AccessLevel.Administrator, new CommandEventHandler( Generate_OnCommand ) );
+			CommandSystem.Register( "Spawn", AccessLevel.Administrator, new CommandEventHandler( Generate_OnCommand ) );
 		}
 
-		[Usage( "SpawnerDev" )]
+		[Usage( "Spawn" )]
 		[Description( "Dev version of the dynamic spawn tool." )]
 		private static void Generate_OnCommand( CommandEventArgs e )
 		{
@@ -32,7 +33,103 @@ namespace Server.Scripts.Commands
 				e.Mobile.SendMessage("You need to provide an action as well as a valid .map name.");
 				return;
 			}
-			Parse( e.Mobile, e.GetString( 0 ).ToLower(), e.GetString( 1 ).ToLower() );
+            else if(e.Length == 1)
+            {
+                if(e.GetString(0) == "export")
+                {
+                    try
+                    {
+                        var baseDir = Core.BaseDirectory;
+                        var export_fileName = Path.Combine(baseDir, "tamere.map");
+                        Console.WriteLine(export_fileName);
+
+                        e.Mobile.SendMessage("Now exporting worldspawns to: " + export_fileName);
+
+                        using (var streamWriter = File.AppendText(export_fileName))
+                        {
+                            var lines = new List<string>();
+                            var spawnCount = 0;
+                            foreach (var item in World.Items)
+                            {
+                                if (item.Value is Spawner)
+                                {
+                                    var currentSpawn = item.Value as Spawner;
+                                    var template = @"+{0} [{1}]: {2} {3} {4} {5} {6} {7} {8} {9} {10}";
+                                    var separator = string.Empty;
+
+                                    var creaturesToSpawn = string.Empty;
+                                    var spawnEntryName = string.Empty;
+                                    // WARNING: this is pre .NET 2.0 code block, later RunUO versions use a true List<string>, not an ArrayList.
+                                    if (currentSpawn.CreaturesName != null)
+                                    {
+                                        // CHECK ALL POSSIBLE CASES
+                                        if(currentSpawn.CreaturesName.Count > 1 && currentSpawn.Count > 1)
+                                            separator = "|";
+                                        else if(currentSpawn.CreaturesName.Count > 1 && currentSpawn.Count == 1)
+                                            separator = ",";
+
+                                        if (currentSpawn.CreaturesName.Count >= 1)
+                                        {
+                                            if (currentSpawn.CreaturesName[0] != null)
+                                                spawnEntryName = currentSpawn.CreaturesName[0].ToString();
+
+                                            foreach (var creature in currentSpawn.CreaturesName)
+                                            {
+                                                creaturesToSpawn += creature.ToString() + separator;
+                                            }
+                                        }
+                                        else if (currentSpawn.CreaturesName.Count == 1)
+                                        {
+                                            if (currentSpawn.CreaturesName[0] != null)
+                                                creaturesToSpawn = currentSpawn.CreaturesName[0].ToString();
+
+
+                                            spawnEntryName += "export_" + creaturesToSpawn;
+                                        }
+                                    }
+                                    if(creaturesToSpawn.LastIndexOf(',') > -1)
+                                        creaturesToSpawn = creaturesToSpawn.Remove(creaturesToSpawn.LastIndexOf(','));
+                                    if(creaturesToSpawn.LastIndexOf('|') > -1)
+                                        creaturesToSpawn = creaturesToSpawn.Remove(creaturesToSpawn.LastIndexOf('|'));
+                                    //
+                                    // Basic format
+                                    // +name [MobileTypeA, MobileTypeB, etc]: X Y MapID NPCCount HomeRange BringToHome MinTime MaxTime Team Description
+                                    // "," can also be "|" depending on spawn being unique or not
+                                    var line = string.Format(template, spawnEntryName, creaturesToSpawn, currentSpawn.X.ToString(), currentSpawn.Y.ToString(), currentSpawn.Map.MapID.ToString(),
+                                        currentSpawn.Count.ToString(), currentSpawn.HomeRange.ToString(), false, currentSpawn.MinDelay.ToString(),
+                                        currentSpawn.MaxDelay.ToString(), currentSpawn.Team.ToString(), string.Empty);
+
+                                    // BringToHome == false because: BringToHome is a Spawn object method telling the spawns to spawn right at the spawn point.
+                                    // It's a Prop in the "Spawn" app only because we need to explicitely tell -or not- whether to bring the spawns home or not
+                                    // during runtime.
+
+                                   // ATTENTION: CHECK EVERYWHERE THAT UNIQUE SPAWNS HAVE , SEPARATORS (UNIQUE == COUNT @ 1 BUT MULTIPLE AVAILABLE MOBS FROM THE SPAWN POOL)
+                                   // CHECK THAT IN THE MAP IMPORTER TOO
+                                   // if (UniqueSpawn)
+                                  //      separator = ", ";
+                                   // else
+                                  //      separator = "| ";
+                                    lines.Add(line);
+                                    spawnCount++;
+                                }
+                            }
+                            foreach (var line in lines)
+                            {
+                                streamWriter.WriteLine(line);
+                            }
+                            e.Mobile.SendMessage("Done exporting " + spawnCount.ToString() + " spawns.");
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+            else
+            {
+                Parse(e.Mobile, e.GetString(0).ToLower(), e.GetString(1).ToLower());
+            }
 		}
 
 		public static void Parse( Mobile from, string action, string spawnFile )
